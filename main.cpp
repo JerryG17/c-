676c6a72ef90b7831affa7fdd75197a7cee94030
gerardog17 <pacoescobedo17@gmail.com>
2019-02-13 22:05:09 -0600

    cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    string nombreC;
+    string nombre;
+    std::cout << "¿Comó te llamas:?" << std::endl; std::cin >> nombre;
+    std::cout << "Así te llamabas" <<nombre<<std::endl;
+    std::cout << "Bien, gracias" << std::endl;
+    std::cout << "Me alegro" << std::end1; 
+    std::cout << "¿Y tu como te llamas?" << std::endl;
+    std::cout << "Perdòn por no presentarme" <<nombreC<< std::end1; std::cin>> nombreC;
+
+
+    return 0;
+}
+
+
